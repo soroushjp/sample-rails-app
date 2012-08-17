10f1e22f4ba703b8cf1abe0fc818f0885f28bb9b
Soroush Pour <soroush@wealthlift.com>
2012-08-17 16:06:05 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '265ef3acb8b0b5925b94f5b4eb46bcae9526078b319ebb1117455c9a6c92191af581fd12949da8f5382f5d723199d1527090178d6826e5e5fff300e93e63e88f'
